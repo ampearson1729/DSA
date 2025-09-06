708f26db341e4c89af3b30b2559ed19e661055d2
Alex Pearson <alexpearson@ucsc-guest-169-233-120-201.ucsc.edu>
2025-09-06 14:53:02 -0700

    Solved Leetcode 55. My answer was in O(n^2) time and was very slow only beat 7.50% of the people.  I think there is a better way than my really slow DP.

---
+++ 55.py
@@ -0,0 +1,35 @@
+class Solution:
+    def canJump(self, nums: list[int]) -> bool:
+        dpt = [False]*len(nums); dpt[-1] = True # Initalize dp table
+        sqrs = nums[:-1]
+        target = len(sqrs)
+        #import pdb; pdb.set_trace()
+        for i, maxJmp in reversed(list(enumerate(sqrs))):
+            for jmp in range(1,maxJmp+1):
+                if i+jmp <= target and dpt[i+jmp]:
+                    dpt[i] = True
+                    break
+        return dpt[0]
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [2,3,1,1,4]
+    print(sol.canJump(nums))
+
+    sol = Solution()
+    nums = [3,2,1,0,4]
+    print(sol.canJump(nums))
+
+
+
+# Example 1:
+
+# Input: nums = [2,3,1,1,4]
+# Output: true
+# Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+# Example 2:
+
+# Input: nums = [3,2,1,0,4]
+# Output: false
+# Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
